8bd17d7ddb36e38f65a939a19bc41ce0cb4a00c9
Tilo Prütz <tilo@pruetz.net>
2019-04-01 21:33:56 +0200

    support challenge with comma separated only (without space)

--- digest.go
+++ digest.go
@@ -97,7 +97,7 @@ func parseChallenge(input string) (*challenge, error) {
 		return nil, ErrBadChallenge
 	}
 	s = strings.Trim(s[7:], ws)
-	sl := strings.Split(s, ", ")
+	sl := strings.Split(s, ",")
 	c := &challenge{
 		Algorithm: "MD5",
 	}
